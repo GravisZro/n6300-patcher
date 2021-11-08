7f2da989be8b2834cf1080ec361704a4223977dd
Luxferre <3335173-suborg@users.noreply.gitlab.com>
2021-11-08 17:34:52 +0000

    Added SELinux spoofing

--- bootpatch.sh
+++ bootpatch.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # assume we have image dir mounted to /image and image/boot.img present
+EARLYBOOT_FILE=init.qcom.early_boot.sh
 cd /image
 cp boot.img boot-orig.img
 echo 'Boot image found, patching...'
@@ -13,7 +14,9 @@ sed -i 's/ro\.secure.*/ro.secure=0/' ./default.prop
 sed -i 's/ro\.debuggable.*/ro.debuggable=1/' ./default.prop
 sed -i 's/.*perf_harden.*/security.perf_harden=0/' ./default.prop
 sed -i '/.*reload_policy.*/d' ./init.rc
-echo 'setenforce 0' >> ./init.qcom.early_boot.sh
+echo 'setenforce 0' >> ./$EARLYBOOT_FILE
+echo 'echo -n 1 > /data/enforce' >> ./$EARLYBOOT_FILE
+echo 'mount -o bind /data/enforce /sys/fs/selinux/enforce' >> ./$EARLYBOOT_FILE
 # initrd root patch process end
 find . | cpio --create --format='newc' | gzip > ../myinitrd.img
 cd ..
