51b308486a1600fa21fc19fa4dbc91ed8288edd6
GravisZro <noreply+github@adaptivetime.com>
2025-06-19 14:50:11 -0400

    Discard Docker requirement

--- bootpatch.sh
+++ bootpatch.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
-# assume we have image dir mounted to /image and image/boot.img present
 EARLYBOOT_FILE=init.qcom.early_boot.sh
-cd /image
-cp boot.img boot-orig.img
-echo 'Boot image found, patching...'
+rm -rf build
+mkdir build
+cp $1 build/boot.img
+cd build
 abootimg -x boot.img
 mkdir initrd
 cd initrd
 cat ../initrd.img | gunzip | cpio -vid
+
 # initrd root patch process start
-cp /home/8k/adbd ./sbin/
-cp /home/8k/slua ./sbin/
-cp /home/8k/sluac ./sbin/
+cp ../../adbd ./sbin/
+cp ../../slua ./sbin/
+cp ../../sluac ./sbin/
 sed -i 's/ro\.secure.*/ro.secure=0/' ./default.prop
 sed -i 's/ro\.debuggable.*/ro.debuggable=1/' ./default.prop
 sed -i 's/.*perf_harden.*/security.perf_harden=0/' ./default.prop
 sed -i '/.*reload_policy.*/d' ./init.rc
-echo 'setenforce 0' >> ./$EARLYBOOT_FILE
-echo 'echo -n 1 > /data/enforce' >> ./$EARLYBOOT_FILE
-echo 'mount -o bind /data/enforce /sys/fs/selinux/enforce' >> ./$EARLYBOOT_FILE
+echo 'setenforce 0' > ./${EARLYBOOT_FILE}
+echo 'echo -n 1 > /data/enforce' >> ./${EARLYBOOT_FILE}
+echo 'mount -o bind /data/enforce /sys/fs/selinux/enforce' >> ./${EARLYBOOT_FILE}
 # initrd root patch process end
+
 find . | cpio --create --format='newc' | gzip > ../myinitrd.img
 cd ..
 # bootimg.cfg patch process start
